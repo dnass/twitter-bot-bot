7d32d89542b35b9080f22ccbe1867fd726214a03
Daniel Nass <dnlnss@gmail.com>
2017-04-27 21:20:32 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const _ = require('lodash');
+const fs = require('fs');
+const Twit = require('twit-promise');
+
+function Bot(run) {
+  this.runFunc = run;
+  const self = this;
+  process.stdin.on('readable', () => {
+    const initData = JSON.parse(process.stdin.read());
+    self.params = initData.params;
+    self.twit = new Twit(initData.twit);
+    self.runFunc().then(() => self.log('kill')).catch(err => self.log({error: err.message}));
+  });
+}
+
+Bot.prototype.log = function (log) {
+  const message = {
+    content: log,
+    date: new Date()
+  }
+  process.stdout.write(JSON.stringify(message) + '\n');
+}
+
+Bot.prototype.tweet = function (tweetData) {
+  const self = this;
+
+  function postTweet(params) {
+    self.log('posting tweet');
+    return self.twit.post('statuses/update', params).then(result => {
+      if (result.data.errors) {
+        throw new Error(JSON.stringify(result.data.errors[0]))
+      } else {
+        self.log('tweet posted successfully')
+        self.log({tweet: result});
+      }
+    }).catch(err =>  {
+      self.log({error: err.message})
+    });
+  }
+
+  if (tweetData.media) {
+    self.log('uploading media');
+    return self.twit.post('media/upload', {
+      media_data: tweetData.media
+    }).then(result => {
+      tweetData.mediaIdStr = result.data.media_id_string;
+      const meta_params = {
+        media_id: tweetData.mediaIdStr,
+        alt_text: {
+          text: tweetData.altText || 'none'
+        }
+      }
+      return self.twit.post('media/metadata/create', meta_params);
+    }).then(() => {
+      const params = {
+        status: tweetData.status,
+        media_ids: [tweetData.mediaIdStr]
+      };
+      return params;
+    }).then(postTweet);
+  } else
+    return postTweet(tweetData);
+}
+
+module.exports = Bot
---
+++ readme.md
@@ -0,0 +1,41 @@
+# twitter-bot-bot
+
+A Node package for building promise-based Twitter bots that can be deployed with [bot-server](https://github.com/dnass/bot-server).
+
+## Installation
+
+`npm install twitter-bot-bot --save`
+
+## Using twitter-bot-bot
+
+### Creating your bot
+
+`var Tbb = require('twitter-bot-bot')`
+
+Initialize the bot by passing the constructor a function containing all of the bot's logic, which returns a promise or promise chain. For example:
+
+`function run() {
+  loadTweets().then(downloadImages).then(postTweet)
+}
+
+var bot = new Tbb(run)`
+
+twitter-bot-bot bots run as child processes of bot-server. For console output, write to `bot.log`.
+
+To post a tweet, use `bot.tweet`. `bot.tweet` takes an object with a `status` value and optional `media` and `altText` values. `media` must be a base64-encoded image. `bot.tweet` returns a promise that resolves to Twitter's response.
+
+### Setting up your bot's package
+
+Bots built with twitter-bot-bot run on [bot-server](https://github.com/dnass/bot-server). To add parameters to your bot that can be configured from the server (such as API keys), add a `botConfig` object to your bot's `package.json` with a params array containing the names of the parameters.
+
+`botConfig: {
+  params: ['NASA_API_KEY']
+}`
+
+You can then access this from your code as `bot.params.NASA_API_KEY`.
+
+The `botConfig` object should also contain a `handle` key with the handle of your Twitter bot's associated account.
+
+### Deploying your bot
+
+For instructions on how to set up a server to run your bot, check out [bot-server](https://github.com/dnass/bot-server).
